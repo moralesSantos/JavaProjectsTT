97de6434ac77f3ebad037b9c001af7a69f8285a9
moralesSantos <49924539+moralesSantos@users.noreply.github.com>
2021-09-12 16:51:31 -0400

    Update fire.js
    hid config details

--- login/firebase-login/src/fire.js
+++ login/firebase-login/src/fire.js
@@ -4,13 +4,7 @@ import 'firebase/firestore';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCpanZrt2uJ3U_6wJ7gskYvxtMgEr3lfvY",
-    authDomain: "login-89579.firebaseapp.com",
-    databaseURL: "https://login-89579-default-rtdb.firebaseio.com/",
-    projectId: "login-89579",
-    storageBucket: "login-89579.appspot.com",
-    messagingSenderId: "472584005500",
-    appId: "1:472584005500:web:771f26e6498a3d3fe7e6f9"
+   //config goes here
   };
 
   const fire = initializeApp(firebaseConfig); 
