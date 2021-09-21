9ef65b9bb402dbc2ccc0e77c749546e0f8b24365
luis <jmsantos879@gmail.com>
2021-09-21 01:28:28 -0400

    adding new file

---
+++ learningTypeScript/test.ts
@@ -0,0 +1,11 @@
+let id: number = 5; 
+let company: string = "traversy media"; 
+let isPublished: boolean = true; 
+
+
+let ids: number[] = [1,2,4,4]; 
+
+//Tuple
+
+let person: [number,string,boolean] = [1,'Maxx',false]; 
+
